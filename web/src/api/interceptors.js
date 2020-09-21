1e19753da1f61b5e4796b3c00147f079735cff2a
QDuy <duypq@pivot.jp>
2020-09-21 22:47:37 +0700

    DATN-12 setting frontend

---
+++ web/src/api/interceptors.js
@@ -0,0 +1,101 @@
+import { useHistory  } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { clearToken } from '../redux/modules/actions/users';
+import { showSpinner, hideSpinner } from '../redux/modules/actions/layout';
+import camelcaseKeys from "camelcase-keys";
+
+const setup = (instance) => {
+  instance.interceptors.request.use(
+    function (config) {
+      const token = useSelector(state => state.users.token) || localStorage.getItem('token');
+      if (token) {
+        config.headers.Authenticator = `Bearer ${token}`;
+      }
+      return config;
+    },
+    function (err) {
+      return Promise.reject(err);
+    }
+  );
+};
+
+const checkToken = (instance) => {
+  instance.interceptors.response.use(
+    (response) => {
+      if (response.headers["content-type"].startsWith("application/json")) {
+        response.data = camelcaseKeys(response.data, { deep: true });
+      }
+      return response;
+    },
+    (error) => {
+      if (
+        (!!sessionStorage.getItem("token") ||
+          !!localStorage.getItem("token")) &&
+        !window.location.pathname.includes("login")
+      ) {
+        // clear token storage
+        useDispatch(clearToken())
+      }
+      return Promise.reject(error);
+    }
+  );
+};
+
+const checkError = (instance) => {
+  instance.interceptors.response.use(
+    (response) => {
+      setTimeout(() => {
+        useDispatch(hideSpinner())
+      }, 500)
+      return response;
+    },
+    (error) => {
+      setTimeout(() => {
+        useDispatch(hideSpinner())
+      }, 500)
+      return Promise.reject(error);
+    }
+  );
+};
+
+const showSpinnerRequest = (instance) => {
+  instance.interceptors.request.use(
+    request => {
+      // set value showSpinner in store
+      useDispatch(showSpinner())
+      return request
+    },
+    error => {
+      // set value showSpinner in store
+      useDispatch(showSpinner())
+      return Promise.reject(error)
+    }
+  )
+}
+
+const hideSpinnerRequest = (instance) => {
+  instance.interceptors.response.use(
+    response => {
+      setTimeout(() => {
+        // set value showSpinner in store
+        useDispatch(hideSpinner())
+      }, 500)
+      return response
+    },
+    error => {
+      setTimeout(() => {
+        // set value showSpinner in store
+        useDispatch(hideSpinner())
+      }, 500)
+      return Promise.reject(error)
+    }
+  )
+}
+
+export default {
+  setup,
+  checkToken,
+  checkError,
+  showSpinnerRequest,
+  hideSpinnerRequest
+};
--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router history={history}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
