5f5d844e16797de90747c4eb9fdbed44be663ab2
Linh, Le Quoc Si <linhlqs@hpt.vn>
2021-01-07 10:39:05 +0700

    Update theaters module

--- server/src/modules/theaters/router.js
+++ server/src/modules/theaters/router.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const handler = require('./handler');
+const {omitBy, isNil} = require('lodash');
 
 router.get('/', (req, res, next) => {
-  let params = {...req.query};
+  let conditions = {
+    _id: req.query._id,
+    name: req.query.name,
+    address: req.query.address,
+    url_image: req.query.url_image,
+    comment: req.query.comment,
+    room_ids: req.query.room_ids
+  };
+  conditions = omitBy(conditions, isNil);
   handler
-    .getList(params)
+    .getList(conditions)
     .then((val) => res.json(val))
     .catch((err) => next(err));
 });
