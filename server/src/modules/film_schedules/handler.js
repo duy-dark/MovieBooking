58a5a235d9ea204e4668fa17b6214bb63d1ae75f
Linh, Le Quoc Si <linhlqs@hpt.vn>
2021-01-30 18:09:13 +0700

    Update nowshowing in schedules module

--- server/src/modules/film_schedules/handler.js
+++ server/src/modules/film_schedules/handler.js
@@ -3,12 +3,50 @@ const resSuccess = require('../../responses/res-success');
 const {omitBy, isNil} = require('lodash');
 const moment = require('moment');
 
-const getNowShowing = async (params) => {
+const getNowShowing = async () => {
   try {
-    let data = await Model.getNowShowing(params);
+    let time_start = new Date(moment());
+
+    let date = new Date(moment().add(1, 'days')).getDate();
+
+    let month = new Date(moment().add(1, 'days')).getMonth();
+    let year = new Date(moment().add(1, 'days')).getFullYear();
+
+    let time_end1 = new Date(
+      moment(
+        `${year}-${month > 8 ? month + 1 : '0' + (month + 1)}-${
+          date > 9 ? date : '0' + date
+        }`,
+        moment.ISO_8601
+      )
+    );
+
+    console.log('time_start: ', time_start);
+    console.log('time_end1:   ', time_end1);
+    console.log('date:       ', date);
+    console.log('month:      ', month + 1);
+    console.log('year:       ', year);
+
+    let lambda = {
+      conditions: {
+        time_start: time_start,
+        time_end1: time_end1,
+        time_end2: new Date(moment(time_end1).add(1, 'days')),
+        time_end3: new Date(moment(time_end1).add(2, 'days')),
+        time_end4: new Date(moment(time_end1).add(3, 'days')),
+        time_end5: new Date(moment(time_end1).add(4, 'days')),
+        time_end6: new Date(moment(time_end1).add(5, 'days')),
+        time_end7: new Date(moment(time_end1).add(6, 'days')),
+        is_deleted: false
+      }
+    };
+
+    let data = await Model.getNowShowing(lambda);
+
     return resSuccess(data);
   } catch (error) {
-    return error;
+    // throw {status: 400, detail: error};
+    throw {status: 400, detail: error};
   }
 };
 
@@ -47,79 +85,6 @@ const findById = async (id) => {
     };
     let data = await Model.findByLambda(lambda);
     return resSuccess(data[0]);
-  } catch (error) {
-    throw {status: 400, detail: error};
-  }
-};
-
-const getFilmToDay = async () => {
-  try {
-    let time_start = new Date(moment().add(7, 'hour'));
-
-    let hour = new Date(moment()).getHours();
-    let date = new Date(moment().add(7, 'hour').add(1, 'days')).getDate();
-    if (hour > 17) {
-      date -= 1;
-    }
-    let month = new Date(moment().add(7, 'hour').add(1, 'days')).getMonth();
-    let year = new Date(moment().add(7, 'hour').add(1, 'days')).getFullYear();
-
-    let time_end = new Date(
-      moment(
-        `${year}-${month > 8 ? month + 1 : '0' + (month + 1)}-${
-          date > 9 ? date : '0' + date
-        }`,
-        moment.ISO_8601
-      ).add(7, 'hour')
-    );
-
-    console.log('time_start: ', time_start);
-    console.log('time_end:   ', time_end);
-    console.log('date:       ', date);
-    console.log('month:      ', month + 1);
-    console.log('year:       ', year);
-
-    let lambda = {
-      conditions: {
-        time_start: time_start,
-        time_end: time_end,
-        is_deleted: false
-      },
-      views: {
-        _id: 1,
-        time_start: 1,
-        time_end: 1,
-        // film_id: 1,
-        films: 1,
-        // theater_id: 1,
-        theater: 1
-        // room: 1
-      }
-    };
-
-    let data = await Model.getFilmToDay(lambda);
-    return resSuccess(data);
-  } catch (error) {
-    // throw {status: 400, detail: error};
-    throw {status: 400, detail: error};
-  }
-};
-
-const getFilm7Day = async (film_id) => {
-  try {
-    let lambda = {
-      conditions: {film_id: film_id, is_deleted: false},
-      views: {
-        _id: 1,
-        time_start: 1,
-        time_end: 1,
-        film_id: 1,
-        theater_id: 1,
-        room: 1
-      }
-    };
-    let data = await Model.findByLambda(lambda);
-    return resSuccess(data);
   } catch (error) {
     throw {status: 400, detail: error};
   }
@@ -185,8 +150,6 @@ module.exports = {
   getNowShowing,
   getList,
   findById,
-  getFilmToDay,
-  getFilm7Day,
   postCreate,
   putUpdate,
   deleteData
--- server/src/modules/film_schedules/router.js
+++ server/src/modules/film_schedules/router.js
@@ -4,49 +4,6 @@ const handler = require('./handler');
 const {omitBy, isNil} = require('lodash');
 const moment = require('moment');
 
-router.get('/getnow', (req, res, next) => {
-  // let gte_match = new Date(moment('01/01/2000', MM / DD / YYYY).format());
-  // let lte_match = new Date(moment('01/01/2050', MM / DD / YYYY).format());
-  // let gte_end = new Date(moment('01/01/2000', MM / DD / YYYY).format());
-  // let lte_end = new Date(moment('09/09/9999', MM / DD / YYYY).format());
-
-  var timezone = Date.now();
-  var timeZone1 = new Date(moment(timezone).format());
-
-  let gte_match = new Date(moment('2015-01-01', moment.ISO_8601));
-  let lte_match = timeZone1;
-  let gte_end = timeZone1;
-  let lte_end = new Date(moment('2080-01-01', moment.ISO_8601));
-
-  let params = {
-    gte_match: gte_match,
-    lte_match: lte_match,
-    gte_end: gte_end,
-    lte_end: lte_end
-  };
-  handler
-    .getNowShowing(params)
-    .then((val) => res.json(val))
-    .catch((err) => next(err));
-});
-
-router.get('/getcomming', (req, res, next) => {
-  var timezone = Date.now();
-  var timeZone1 = new Date(moment(timezone).format());
-
-  let gte_match = timeZone1;
-  let lte_match = new Date(moment('2080-01-01', moment.ISO_8601));
-
-  let params = {
-    gte_match: gte_match,
-    lte_match: lte_match
-  };
-  handler
-    .getComingSoon(params)
-    .then((val) => res.json(val))
-    .catch((err) => next(err));
-});
-
 router.get('/', (req, res, next) => {
   let conditions = {
     _id: req.query._id,
@@ -63,25 +20,17 @@ router.get('/', (req, res, next) => {
     .catch((err) => next(err));
 });
 
+router.get('/nowshowing', (req, res, next) => {
+  handler
+    .getNowShowing()
+    .then((val) => res.json(val))
+    .catch((err) => next(err));
+});
+
 router.get('/detail/:id', (req, res, next) => {
   let id = req.params.id;
   handler
     .findById(id)
-    .then((val) => res.json(val))
-    .catch((err) => next(err));
-});
-
-router.get('/getfilmtoday', (req, res, next) => {
-  handler
-    .getFilmToDay()
-    .then((val) => res.json(val))
-    .catch((err) => next(err));
-});
-
-router.get('/getfilm7day/:film_id', (req, res, next) => {
-  let film_id = req.params.film_id;
-  handler
-    .getFilm7Day(film_id)
     .then((val) => res.json(val))
     .catch((err) => next(err));
 });
