9dddfa0bdd90b8711adc4c5283561bbdd0df23ca
QDuy <duypq@pivot.jp>
2021-02-25 10:44:04 +0700

    update api comment

--- web/src/api/films.js
+++ web/src/api/films.js
@@ -29,7 +29,7 @@ const getSeats = (id) => {
 }
 
 const getComments = (params) => {
-  return Api.get(`/api/film_comment/${params}`).then(res => res.data)
+  return Api.get(`/api/film_comment?film_id=${params}`).then(res => res.data)
 }
 
 const search = () => {
