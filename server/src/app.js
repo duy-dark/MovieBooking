c1f6d7b1960b06138df86dfd65ccfa609c93bb4b
Linh, Le Quoc Si <linhlqs@hpt.vn>
2021-03-02 18:09:22 +0700

    Update

--- server/src/app.js
+++ server/src/app.js
@@ -9,12 +9,22 @@ const express = require('express'),
   resFail = require('./responses/res-fail'),
   config = require('./config');
 
+const socketio = require('socket.io');
 const {port} = config;
 
 const app = express();
 const swaggerDocument = require('./swagger.json');
 const swaggerUI = require('swagger-ui-express');
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+//require socket server
+const connection = require('./util/socket_booking');
+const server = require('http').createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
 // app.use(authen) check token
 app.use(bodyParser.json());
@@ -73,6 +83,9 @@ app.use((req, res) => {
 });
 
 // app.use() error
+
+// call connection socket
+io.on('connection', (socket) => connection(socket, io));
 
 const startSever = async () => {
   app.listen(port, async () => {
