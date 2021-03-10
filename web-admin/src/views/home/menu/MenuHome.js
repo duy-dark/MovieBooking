b8d876f4f776472a21f4fa706c5ac3f4901c6af7
NGUYỄN NGÔ TRẦN <trannguyen3398@gmail.com>
2021-03-10 10:17:36 +0700

    update code

--- web-admin/src/views/home/menu/MenuHome.js
+++ web-admin/src/views/home/menu/MenuHome.js
@@ -36,9 +36,9 @@ export default function MenuHome(props) {
         </div>
         <SubMenu key="sub1" icon={<AppstoreOutlined />} title="Phim">
       <Menu.Item key="sub1-1">Danh sách phim   <Link to="/ManageFilm" /></Menu.Item>
-          <Menu.Item key="sub1-2">Thể loại</Menu.Item>
-          <Menu.Item key="sub1-3"><Link to="/theater/list">Rạp</Link></Menu.Item>
-          <Menu.Item key="sub1-4"><Link to="/new/list">New</Link></Menu.Item>
+        
+          <Menu.Item key="sub1-2"><Link to="/theater/list">Rạp</Link></Menu.Item>
+          <Menu.Item key="sub1-3"><Link to="/new/list">New</Link></Menu.Item>
         </SubMenu>
       
          
