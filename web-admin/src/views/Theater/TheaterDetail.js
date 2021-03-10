bdea3b5cfe09b0a37462657b1d10e8fdd0305480
NGUYỄN NGÔ TRẦN <trannguyen3398@gmail.com>
2021-03-11 01:36:13 +0700

    thong ke

--- web-admin/src/views/Theater/TheaterDetail.js
+++ web-admin/src/views/Theater/TheaterDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getTheaterDetail, updateTheater, deleteRoom, getTheaterChart } from "../../redux/films/actions"
+import { getTheaterDetail, updateTheater, deleteRoom, getTheaterChart,getListFilmNow } from "../../redux/films/actions"
 import { Link } from 'react-router-dom'
 import { useDispatch , useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
@@ -7,18 +7,82 @@ import httpFilm from '../../api/films';
 import "../../styles/Theater/detail.scss";
 import ButtonEdit from '../../components/ButtonEdit'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import {  Select} from 'antd';
+import {CanvasJSChart} from 'canvasjs-react-charts'
+import FormItemInput from 'antd/lib/form/FormItemInput';
 
+const { Option } = Select;
+const optionStatistic = [
+  {value : "Ngày", id:1},
+  {value : "Tuần",id:2},
+ { value : "Tháng",id:3},
+ { value : "Năm", id:4}
+
+]
+
+    const formatdata=(filmsNow,data)=>{
+      let films=filmsNow.map(item=>({"id":item._id,"name":item.name}))
+  let filmsid=films.map(item=>item.id)
+  let hello1=films.map(item=>({label:item.name, y: 0}))
+  data.map(item=>{
+      let index = filmsid.indexOf(item.film_schedules.film_id)
+      if(filmsid.includes(item.film_schedules.film_id)){
+       hello1[index] = {...hello1[index],y:item.count + hello1[index].y}
+      }
+})
+
+return hello1;
+}
+const failureandsuccess=(filmsNow,failure,success)=>{
+     let films=filmsNow.map(item=>({"id":item._id,"name":item.name}))
+  let filmsid=films.map(item=>item.id)
+  let hello1=[{label:"success",y:0},{label:"falure",y:0}];
+  failure.map(item=>{
+    let index = filmsid.indexOf(item.film_schedules.film_id)
+    if(filmsid.includes(item.film_schedules.film_id)){
+      hello1[1].y+=item.count
+    }
+  })
+  success.map(item=>{
+    let index = filmsid.indexOf(item.film_schedules.film_id)
+    if(filmsid.includes(item.film_schedules.film_id)){
+      hello1[0].y+=item.count
+    }
+  })
+  return hello1
+}
 export default function TheaterDetail() {
   const [isEdit, setIsEdit] = useState(false);
   const dispatch = useDispatch();
   const theaterDetail = useSelector((state) => state.films.theaterDetail)
+   const filmsNow = useSelector((state) => state.films.filmsNow);
   const theaterChart = useSelector((state) => state.films.theaterChart)
+  const [chartData,setChartData]= useState([])
+  const [chartDataCompare,setChartDataCompare]= useState([])
+ 
+
+  useEffect(()=>{
+    const {success=[],failure=[]}=theaterChart
+    if(success.length>0){
+    
+    let arr=formatdata(filmsNow,success)
+    arr=arr.filter(item=>item.y>0)
+    setChartData(arr)
+    }
+    if(failure.length>0){
+    let failarr=failureandsuccess(filmsNow,failure,success)
+    failarr=failarr.filter(item=>item.y>0)
+    setChartDataCompare(failarr)
+    }
+  },[theaterChart])
   const [theater, setTheater] = useState({});
   const [rooms, setRooms] = useState([]);
   const { id } = useParams();
+  const [type,setType]=useState(1)
   useEffect(() => {
     dispatch(getTheaterDetail(id))
     dispatch(getTheaterChart({id: id, type: 1}))
+    dispatch(getListFilmNow());
   }, [])
 
   useEffect(() => {
@@ -50,7 +114,80 @@ export default function TheaterDetail() {
   const onDeleteRoom = (id) => {
     dispatch(deleteRoom(id))
   }
+  const datachart=
+	{
+		title: {
+			text: ""
+		},
+		toolTip: {
+			shared: true,
+			contentFormatter: function (e) {
+				var content = " ";
+				for (var i = 0; i < e.entries.length; i++) {
+					content += e.entries[i].dataPoint.label + " " + "<strong>" + e.entries[i].dataPoint.y + "</strong>";
+					content += "<br/>";
+				}
+				return content;
+			}
+		},
+		axisX: {
+			gridColor: "Silver",
+	
+		},
+		axisY: {
+			includeZero: false
+		},
+		data: [
+		{
+		
+			type: "column",
+			showInLegend: true,
+			name: "Unique Visits",
+			dataPoints: chartData
+		}
+		]
+  }
+  const datachart1=
+	{
+		title: {
+			text: ""
+		},
+		toolTip: {
+			shared: true,
+			contentFormatter: function (e) {
+				var content = " ";
+				for (var i = 0; i < e.entries.length; i++) {
+					content += e.entries[i].dataPoint.label + " " + "<strong>" + e.entries[i].dataPoint.y + "</strong>";
+					content += "<br/>";
+				}
+				return content;
+			}
+		},
+		axisX: {
+			gridColor: "Silver",
+	
+		},
+		axisY: {
+			includeZero: false
+		},
+		data: [
+		{
+		
+			type: "pie",
+			showInLegend: true,
+			name: "Unique Visits",
+			dataPoints: chartDataCompare
+		}
+		]
+  }
+const getStatistic = (value,record)=>{
 
+  setType(record.id)
+  dispatch(getTheaterChart({id: id, type: record.id}))
+  
+ 
+  
+}
   return (
     <div className="layout-detail layout-detail__theater">
       <Tabs className="layout-ddt">
@@ -59,8 +196,14 @@ export default function TheaterDetail() {
           <Tab className="layout-ddt__header__item">Thông tin</Tab>
         </TabList>
         <TabPanel className="layout-ddt__content">
-          <div className="layout-theater__right">
-            
+          <div style={{width:100}}> 
+            <Select options={optionStatistic} defaultValue={'Ngày'} onChange={getStatistic}/>
+          </div>
+          
+          <CanvasJSChart options = {datachart}/>
+          
+          <div>
+          <CanvasJSChart options = {datachart1}/> 
           </div>
         </TabPanel>
         <TabPanel className="layout-ddt__content">
